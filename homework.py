8f547d894129ccbcd652d46fc6b336df485193a4
Meline Sarkisyan <s.m.m21-08-90@mail.ru>
2022-07-21 15:42:32 +0300

    My first project

--- homework.py
+++ homework.py
@@ -1,23 +1,29 @@
+from dataclasses import asdict, dataclass
+
+
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self, training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
-
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    сalories: float
+    text_message=('Тип тренировки: {training_type}; '
+                 'Длительность: {duration:3f} ч.; '
+                 'Дистанция: {self.distance:.3f} км; '
+                 'Ср.скорость: {self.speed:.3f} км/ч; '
+                 'Потрачено ккал: {self.calories:.3f}. '
+                 )
 
     def get_message(self) -> str:
-        return(f'Тип тренировки: {self.training_type}; '
-               f'Длительность: {self.duration:.3f} ч.; '
-               f'Дистанция: {self.distance:.3f} км; '
-               f'Ср.скорость: {self.speed:.3f} км/ч; '
-               f'Потрачено ккал: {self.calories:.3f}. ')
+        return self.text_message.format(
+            training_type=self.training_type,
+            duration= self.duration,
+            distance=self.distance,
+            speed=self.speed,
+            calories=self.calories,
+        )
 
 
 class Training:
