38041cb327eef299042ac4cf4878563355c5d9e7
Meline Sarkisyan <s.m.m21-08-90@mail.ru>
2022-07-21 23:06:17 +0300

    First commit: change program.py

--- homework.py
+++ homework.py
@@ -142,12 +142,14 @@ def main(training: Training) -> None:
     """Главная функция."""
     info = training.show_training_info()
     print(info.get_message())
-    
+
+
 if __name__ == '__main__':
-    packages = [('SWM', [720, 1, 80, 25, 40]),
-               ('RUN', [15000, 1, 75]),
-               ('WLK', [9000, 1, 75, 180]),
-               ]
+    packages = [(
+        'SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
 
     for workout_type, data in packages:
         training = read_package(workout_type, data)
