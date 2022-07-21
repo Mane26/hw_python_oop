9e610db6aef2d49b4fe4e0b0ecf7bd4cbf678307
Meline Sarkisyan <s.m.m21-08-90@mail.ru>
2022-07-21 22:48:35 +0300

    First commit: change program.py

--- homework.py
+++ homework.py
@@ -143,12 +143,11 @@ def main(training: Training) -> None:
     info = training.show_training_info()
     print(info.get_message())
     
-
 if __name__ == '__main__':
     packages = [('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
-    ]
+               ('RUN', [15000, 1, 75]),
+               ('WLK', [9000, 1, 75, 180]),
+               ]
 
     for workout_type, data in packages:
         training = read_package(workout_type, data)
